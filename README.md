c7a55ae4840020ea8b8cef861fc7e1749e37267c
z94snq <z94snq@gmail.com>
2026-01-11 06:07:50 +0900

    add initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # gbdk-handling-sprite
 GBDK sprite animation demo with bouncing ball physics, similar to the classic "bouncing DVD logo" screensaver
+
+## Build Instructions
+Place gbdk for Windows in the `C:\gbdk` directory, then run this command in PowerShell:
+
+```
+C:\gbdk\bin\lcc.exe -c -o main.o main.c
+C:\gbdk\bin\lcc.exe -c -o BallSprite.o BallSprite.c
+C:\gbdk\bin\lcc.exe -o rom.gb main.o BallSprite.o
+```
+
+or just run `build.ps1`
+
+This will create `rom.gb`, which can be loaded into an emulator or flashcart.
+
+## Convert PNG into GBDK asset
+```
+C:\gbdk\bin\png2asset.exe .\BallSprite.png -spr8x8
+```
+
+## Resources
+- GBDK: https://github.com/gbdk-2020/gbdk-2020
+- Aseprite: https://www.aseprite.org/
+- png2asset Tutorial: https://laroldsretrogameyard.com/tutorials/gb/png2asset-graphics-creation-for-gbdk-2020
+- Sprite Handling Tutorial: https://laroldsretrogameyard.com/tutorials/gb/drawing-and-moving-sprites-in-gameboy-games/
---
+++ main.c
@@ -0,0 +1,79 @@
+#include <gb/gb.h>
+#include "BallSprite.h"
+
+uint8_t spriteX, spriteY;
+int8_t velocityX, velocityY;
+
+void main(void)
+{
+    DISPLAY_ON;
+    SHOW_SPRITES;
+
+    set_sprite_data(0, 1, BallSprite_tiles);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 84, 88);
+
+    // Set default position
+    spriteX = 80;
+    spriteY = 72;
+
+    // Set default velocity
+    velocityX = 1;
+    velocityY = 1;
+
+    // Loop forever
+    while(1){
+        // Apply velocity
+        spriteX += velocityX;
+        spriteY += velocityY;
+
+        // Check if sprite is at right screen edge
+        if((spriteX > 156) && (velocityX > 0)){
+
+            // Switch movement direction
+            velocityX = -velocityX;
+
+            // Clamp X position within screen edge
+            spriteX = 156;
+        }
+
+        // Check if sprite is at bottom screen edge
+        if((spriteY > 140) && (velocityY > 0)){
+
+            // Switch movement direction
+            velocityY = -velocityY;
+
+            // Clamp Y position within screen edge
+            spriteY = 140;
+        }
+
+        // Check if sprite is at left screen edge
+        if((spriteX < 4) && (velocityX < 0)){
+
+            // Switch movement direction
+            velocityX = -velocityX;
+
+            // Clamp X position within screen edge
+            spriteX = 4;
+        }
+
+        // Check if sprite is at top screen edge
+        if((spriteY < 4) && (velocityY < 0)){
+
+            // Switch movement direction
+            velocityY = -velocityY;
+
+            // Clamp Y position within screen edge
+            spriteY = 4;
+        }
+
+        // Position the first sprite at our spriteX and spriteY
+        // All sprites are render 8 pixels to the left of their x position and 16 pixels ABOVE their actual y position
+        // This means an object rendered at 0,0 will not be visible
+        // x+5 and y+12 will center the 8x8 tile at our x and y position
+        move_sprite(0, spriteX + 4, spriteY + 12);
+
+        // Done processing, yield CPU and wait for start of next frame
+        wait_vbl_done();
+    }
+}
